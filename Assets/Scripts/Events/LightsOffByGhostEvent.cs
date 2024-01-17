0d867892d8d35a75737e4629a458df20aab6db79
Sujit Reddy <Sujitreddy97@users.noreply.github.com>
2024-01-17 16:29:16 +0530

    Coding EventTrigger of LightsTurnOffByGhostEvent
    - Code LightsOffByGhostEvent script
    - Check for the appropriate keys required to trigger the event
    - Invoke the LightsOffByGhostEvent and play spooky giggle sound

--- Assets/Scripts/Events/LightsOffByGhostEvent.cs
+++ Assets/Scripts/Events/LightsOffByGhostEvent.cs
@@ -2,5 +2,14 @@ using UnityEngine;
 
 public class LightsOffByGhostEvent : MonoBehaviour
 {
-
+    [SerializeField] private int keysRequired;
+    [SerializeField] private SoundType soundType;
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.GetComponent<PlayerView>() != null && GameService.Instance.GetPlayerController().KeysEquipped == keysRequired)
+        {
+            EventService.Instance.OnLightsOffByGhostEvent.InvokeEvent();
+            GameService.Instance.GetSoundView().PlaySoundEffects(soundType);
+        }
+    }
 }
